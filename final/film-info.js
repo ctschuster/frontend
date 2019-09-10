d2aa4d4b8ab9a03d2ac18558375617c9c38a05e1
Chris Schuster <chris@schuster.io>
2019-09-09 21:30:22 -0500

    completed final project for intro to web development - final-info form project

--- final/film-info.js
+++ final/film-info.js
@@ -1,9 +1,10 @@
 function validateForm() {
     if (!checkFilmName())    return false;
+    if (!checkYear())        return false;
     if (!checkSynopsis())    return false;
-    if (!validateReview())   return false;
-    if (!validateLength())   return false;
-    if (!ValidateGenre())    return false;
+    if (!checkReview())      return false;
+    if (!checkLength())      return false;
+    if (!checkGenre())       return false;
     return true;
 }
 
@@ -13,6 +14,7 @@ function checkFilmName() {
     if (filmname.trim().length == 0) {
         document.getElementById("namestatus").innerHTML   = "enter film name";
         document.getElementById("namestatus").style.color = "red";
+        return false;
     } else {
         document.getElementById("namestatus").innerHTML   = "";
         document.getElementById("namestatus").style.color = "black";
@@ -20,11 +22,25 @@ function checkFilmName() {
     return true;
 }
 
+function checkYear() {
+    var year = Number(document.getElementById("year").value);
+    if (year < 1895 || year > 2022) {
+        document.getElementById("yearstatus").innerHTML   = "enter release year";
+        document.getElementById("yearstatus").style.color = "red";
+        return false;
+    } else {
+        document.getElementById("yearstatus").innerHTML   = "";
+        document.getElementById("yearstatus").style.color = "black";
+    }
+    return true;
+}
+
 function checkSynopsis() {
     var reviewtext = document.getElementById("synopsis").value;
-    if (reviewtext.trim().length == 0) {
-        document.getElementById("synstatus").innerHTML   = "enter brief synopsis";
+    if (reviewtext.trim().length < 20) {
+        document.getElementById("synstatus").innerHTML   = "enter brief synopsis (20+ chars)";
         document.getElementById("synstatus").style.color = "red";
+        return false;
     } else {
         document.getElementById("synstatus").innerHTML   = "";
         document.getElementById("synstatus").style.color = "black";
@@ -32,11 +48,12 @@ function checkSynopsis() {
     return true;
 }
 
-function validateReview() {
+function checkReview() {
     var reviewtext = document.getElementById("review").value;
-    if (reviewtext.trim().length == 0) {
-        document.getElementById("reviewstatus").innerHTML   = "enter your review";
+    if (reviewtext.trim().length < 100) {
+        document.getElementById("reviewstatus").innerHTML   = "enter your review (100+ chars)";
         document.getElementById("reviewstatus").style.color = "red";
+        return false;
     } else {
         document.getElementById("reviewstatus").innerHTML   = "";
         document.getElementById("reviewstatus").style.color = "black";
@@ -44,9 +61,17 @@ function validateReview() {
     return true;
 }
 
-function validateLength() {
-    document.getElementById("lengthstatus").innerHTML = "";
-    var len = document.getElementById("length").trim().value;
+function checkLength() {
+    var len = Number(document.getElementById("length").value);
+    if (len < 1 || len > 240) {
+        document.getElementById("lentype").innerHTML      = "";
+        document.getElementById("lengthstatus").innerHTML   = "enter run time (1-240 minutes)";
+        document.getElementById("lengthstatus").style.color = "red";
+        return false;
+    } else {
+        document.getElementById("lengthstatus").innerHTML   = "";
+        document.getElementById("lengthstatus").style.color = "black";
+    }
     if (len<=40) {
         document.getElementById("lentype").innerHTML = "shorts section";
     } else {
@@ -55,6 +80,22 @@ function validateLength() {
     return true;
 }
 
-function ValidateGenre() {
+function checkGenre() {
+    var genresSelected =
+        document.getElementById("genreDoc").checked +
+        document.getElementById("genreScifi").checked +
+        document.getElementById("genreHorror").checked +
+        document.getElementById("genreDrama").checked +
+        document.getElementById("genreComedy").checked +
+        document.getElementById("genreAction").checked;
+    if (genresSelected < 1) {
+        document.getElementById("genrestatus").innerHTML    = "select at least one genre";
+        document.getElementById("genrestatus").style.color  = "red";
+        return false;
+    } else {
+        document.getElementById("genrestatus").innerHTML   = "";
+        document.getElementById("genrestatus").style.color = "black";
+    }
+
     return true;
 }
